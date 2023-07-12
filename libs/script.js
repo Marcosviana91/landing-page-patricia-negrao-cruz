8d4468f522c9fc9de46a2357ba3e4371aeaa2be6
Marcos Viana <97001316+Marcosviana91@users.noreply.github.com>
2023-07-11 21:47:18 -0300

    estilização do formulário

--- libs/script.js
+++ libs/script.js
@@ -1,3 +1,32 @@
 $(document).ready(function () {
+    $('#tel').mask('(00) 0.0000-0000');
+
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (() => {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            const forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.from(forms).forEach(form => {
+                form.addEventListener('submit', event => {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+                    else {
+                        let nome = document.getElementById("nome")
+                        let mensagem = document.getElementById("mensagem")
+                        alert(nome.value + " " + mensagem.value)
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+                form.addEventListener('reset', event => {
+                    form.classList.remove('was-validated')
+                })
+            })
+        })();
 
 });
